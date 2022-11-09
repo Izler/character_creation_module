6fe38a7e4f0d7cc1e48b92dcc5c158584a49f24a
Aleksei K <Difomor@gmail.com>
2022-11-09 13:08:28 +0300

    Added module asciimatics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from graphic_arts.start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
@@ -80,6 +80,7 @@ def choice_char_class() -> str:
 
 
 def main():
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -91,5 +92,4 @@ def main():
     print(start_training(char_name, char_class))
 
 
-# main()
-
+main()
